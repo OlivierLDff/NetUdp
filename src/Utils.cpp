3d6c10ae686b58262a4d58e79c35bff29e203c9b
Olivier LDff <olivier.ldff@gmail.com>
2019-12-12 13:11:38 +0100

    Better Registration

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -34,8 +34,10 @@ static const char** _uri = &_defaultUri;
 static quint8 _major = 1;
 static quint8 _minor = 0;
 
-static void registerTypes()
+static void NetUdp_registerTypes()
 {
+    qCDebug(NETUDP_UTILS_LOG_CAT, "Register NetUdp v%s", qPrintable(NetUdp::Version::version().readable()));
+
     qCDebug(NETUDP_UTILS_LOG_CAT, "Register Singleton %s.Version %d.%d to QML", *_uri, _major, _minor);
     NETUDP_NAMESPACE::Version::registerSingleton(*_uri, _major, _minor);
 
@@ -46,25 +48,34 @@ static void registerTypes()
     NETUDP_NAMESPACE::Datagram::registerType();
 }
 
-static void registerTypes(const char* uri, const quint8 major, const quint8 minor)
+static void NetUdp_registerTypes(const char* uri, const quint8 major, const quint8 minor)
 {
     if(uri)
         _uri = &uri;
     _major = major;
     _minor = minor;
-    registerTypes();
+    NetUdp_registerTypes();
 }
 
-Q_COREAPP_STARTUP_FUNCTION(registerTypes)
+void NetUdp_loadResources()
+{
+    qCDebug(NETUDP_UTILS_LOG_CAT, "Load NetUdp.qrc v%s", qPrintable(NetUdp::Version::version().readable()));
+    Q_INIT_RESOURCE(NetUdp);
+}
+
+#ifndef NETUDP_STATIC
+Q_COREAPP_STARTUP_FUNCTION(NetUdp_registerTypes)
+Q_COREAPP_STARTUP_FUNCTION(NetUdp_loadResources)
+#endif
 
 NETUDP_USING_NAMESPACE;
 
 void Utils::registerTypes(const char* uri, const quint8 major, const quint8 minor)
 {
-    ::registerTypes(uri, major, minor);
+    ::NetUdp_registerTypes(uri, major, minor);
 }
 
 void Utils::loadResources()
 {
-    Q_INIT_RESOURCE(NetUdp);
+    ::NetUdp_loadResources();
 }
