93d82a0b25f1362beebdfbfafeb61051ad63f8e0
Olivier LDff <olivier.ldff@gmail.com>
2020-04-13 11:52:11 +0200

    _separateRxTxSockets only if separateRxTxSockets() || txPort()

--- src/Server.cpp
+++ src/Server.cpp
@@ -110,7 +110,7 @@ bool Server::start()
     for (const auto& it : multicastGroupsSet())
         _worker->_multicastGroups.insert(it, false);
 
-    _worker->_separateRxTxSockets = separateRxTxSockets() || txPort() || multicastLoopback();
+    _worker->_separateRxTxSockets = separateRxTxSockets() || txPort();
     _worker->_multicastInterface = QNetworkInterface::interfaceFromName(multicastInterfaceName());
     _worker->_multicastLoopback = multicastLoopback();
     _worker->_inputEnabled = inputEnabled();
