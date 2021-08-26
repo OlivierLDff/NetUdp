c1d1d814f13aaf39f7334c252bf080c1eb2ead83
Olivier LDff <olivier.ldff@gmail.com>
2021-08-26 15:55:21 +0200

    🐛 Use steady_clock instead of system to avoid rollback

--- src/NetUdp/InterfacesProvider.cpp
+++ src/NetUdp/InterfacesProvider.cpp
@@ -83,7 +83,7 @@ class QRealNetworkIFaceProvider : public InterfacesProvider::IProvider
 public:
     static std::uint64_t now()
     {
-        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
     }
 
     mutable std::uint64_t lastCacheFetch = 0;
